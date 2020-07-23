045c9032740912e969db93c3f86ac351beb8f575
csabourin <christian.sabourin@canada.ca>
2020-07-23 10:23:33 -0400

    Initial commit

---
+++ settings/settings-general.js
@@ -0,0 +1,10 @@
+'use strict';
+define({
+  "courseLegacyCode": "G140",
+  "courseTitle_en": "Performance Management for the Government of Canada (G140)",
+  "courseTitle_fr": "Gestion du rendement pour le gouvernement du Canada (G140)",
+  "seriesTitle_en": "",
+  "seriesTitle_fr": "",
+  "addToolboxPage": [],
+  "editMode": true
+});
