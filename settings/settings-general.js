3f98cc55e0746527a69bf01969ebb29ad2f0fef1
csabourin <christian.sabourin@canada.ca>
2020-09-18 10:59:09 -0400

    Inserted downloadable courses documents

--- settings/settings-general.js
+++ settings/settings-general.js
@@ -5,7 +5,7 @@ define({
   "courseTitle_fr": "Gestion du rendement pour le gouvernement du Canada (G140)",
   "seriesTitle_en": "",
   "seriesTitle_fr": "",
-  "addToolboxPage": [{"name_en":"Download course", "name_fr":"Télécharger le cours", "filename_en":"print_version.pdf", "filename_fr":"print_version_fr.pdf"}],
+  "addToolboxPage": [{"name_en":"Download course", "name_fr":"Cours à télécharger", "filename_en":"Manual_G140_Printable.docx", "filename_fr":"Manuel_G140_imprimable.docx"}],
   showResources: true,
   "editMode": false
 });
